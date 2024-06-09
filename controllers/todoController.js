9f12c5d72a096eb66e7cd38faf03a5abfbb73e0a
Serhii Yatsenko <serhii2111@yahoo.com>
2024-06-09 23:56:03 +0300

    card-almost

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,6 @@
 import Todo from "../models/todoModel.js";
 
-import { createTodoSchema } from "../schemas/todoSchema.js";
-
+import { createTodoSchema } from "../schemas/todoSchema.js"; 
 // Логіка пов'язана з контроллерами задач (Todo):
 
 // createTodo: Створює нову задачу для колонки та повертає її у відповіді.
@@ -9,17 +8,14 @@ import { createTodoSchema } from "../schemas/todoSchema.js";
 // 2. Викликає сервіс для додавання нової задачі з даними з тіла запиту та ідентифікатором колонки.
 // 3. Відповідає з статусом 201 та новоствореною задачею.
 
-export const createTodo = async (req, res, next) => {  
-  // ...
-
+export const createTodo = async (req, res, next) => {    
   const todo = {
     title: req.body.title,
     description: req.body.description,
     priority: req.body.priority,
     deadline: req.body.deadline,
-    // ідентифікатор юзера який створює ц. картку,
-    // при створенні картки беремо id користувача з jwt- токена
-    // ownerColumn: req.column.id,
+    // ідентифікатор column в якій створюємо картку, при цьому беремо id column з запита на фронтенді
+    ownerColumn: req.params.columnId,
   };
 
   try {
@@ -35,11 +31,10 @@ export const createTodo = async (req, res, next) => {
 // 2. Викликає сервіс для отримання списку всіх дошок користувача.
 // 3. Відповідає з статусом 200 та списком дошок.
 
-export const getAllTodos = async (req, res, next) => {
-  // ...
+export const getAllTodos = async (req, res, next) => {   
   try {
-    // запит всіх Boards в колекції
-    const result = await Todo.find();
+    // запит всіх Todos in column 
+    const result = await Todo.find({ ownerColumn: req.params.columnId });
 
     res.status(200).json(result);
   } catch (error) {
@@ -50,21 +45,20 @@ export const getAllTodos = async (req, res, next) => {
 // getById: Отримує задачу за ідентифікатором та повертає її у відповіді.
 // 1. Отримує ідентифікатор задачі з параметрів запиту.
 // 2. Викликає сервіс для отримання задачі за ідентифікатором.
-// 3. Відповідає з статусом 200 та знайденою задачею.
+// 3. Відповідає з статусом 200 та знайденою задачею. 
 
-// 
-export const getOneTodo = async (req, res, next) => {
-  // ...
+export const getOneTodo = async (req, res, next) => {  
+  // отримуємо ідентифікаторu Column з id
+  const { columnId, todoId } = req.params;
 
-  // отримуємо ідентифікатор Column з id
-  const { todoId } = req.params;
-
-  // потрібно додати Joi валідацію значень полів щодо id на тип ObjectId !!!!!!!!
-
+  // потрібно додати Joi валідацію значень полів щодо id на тип ObjectId !!!!!!!! 
   try {
     //  пошук з ідентифікатором   - якшо немає такого id - findById повертає null
-    const result = await Todo.findById(todoId);
-    //  обробка помилки - якщо контакт не знайдено
+    const result = await Todo.findById({
+      ownerColumn: columnId,
+      _id: todoId,
+    });
+    //  обробка помилки - якщо card не знайдено
     if (result === null) {
       return res.status(404).json({ message: "Not found" });
     }
@@ -81,12 +75,15 @@ export const getOneTodo = async (req, res, next) => {
 // 3. Відповідає з статусом 200 та видаленою задачею.
 
 export const deleteTodo = async (req, res, next) => {   
-  // отримуємо ідентифікатор картки з id
-  const { todoId } = req.params; 
-  console.log(req.params);
+  // отримуємо ідентифікаторu картки
+  const { columnId, todoId } = req.params; 
+  
   try {
     // якщо треба видалити не за id - метод findOneAndDelete({name: "Iv"} ) та зазначити по якому полю 
-    const result = await Todo.findByIdAndDelete(todoId);
+    const result = await Todo.findByIdAndDelete({
+      ownerColumn: columnId,
+      _id: todoId,
+    });
     // перевірка - обробка помилки - якщо картку не знайдено
     if (result === null) {
       return res.status(404).json({ message: "Not found" });
@@ -98,28 +95,31 @@ export const deleteTodo = async (req, res, next) => {
   }
 };
 
-// updateTodo: Оновлює поточну задачу за ідентифікатором та повертає оновлену задачу у відповіді.
+// updateTodo: Оновлює поточну card in column за ідентифікатором та повертає оновлену card
 // 1. Отримує ідентифікатор задачі з параметрів запиту.
 // 2. Викликає сервіс для оновлення задачі за ідентифікатором та даними з тіла запиту.
 // 3. Відповідає з статусом 200 та оновленою задачею.
 
-export const updateTodo = async (req, res, next) => {
-  // ...
-  const { todoId } = req.params;
+export const updateTodo = async (req, res, next) => {   
+  const { columnId, todoId } = req.params;
 
   const todo = {
     title: req.body.title,
     description: req.body.description,
     priority: req.body.priority,
     deadline: req.body.deadline,
-    // ідентифікатор юзера який створює ц. картку,
-    // при створенні картки беремо id користувача з jwt- токена
-    // ownerColumn: req.column.id,
+    // ідентифікатор column в який оновлюється card
+    ownerColumn: req.params.columnId,
   };
 
   try {
-    // щоб findByIdAndUpdate ПОВЕРНУВ актуальну(нову а не стару) версію документа треба додати { new: true} -
-    const result = await Todo.findByIdAndUpdate(todoId, todo, {
+    // щоб findByIdAndUpdate ПОВЕРНУВ актуальну(а не стару) версію документа треба додати { new: true} -
+    const result = await Todo.findByIdAndUpdate(
+      {
+      ownerColumn: columnId,
+      _id: todoId,
+      }, todo,
+      {
       new: true,
     });
     // перевірка - обробка помилки - якщо board не знайдено
